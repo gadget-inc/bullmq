31cc290d17ff32d71cb7f7619c636e5d6cbafe1d
Jason Gedge <jason@gadget.dev>
2022-04-22 13:45:42 -0400

    gitpkg

---
+++ dist/interfaces/advanced-options.d.ts
@@ -0,0 +1,7 @@
+export interface AdvancedOptions {
+    /**
+     * A set of custom backoff strategies keyed by name.
+     */
+    backoffStrategies?: {};
+}
+export declare const AdvancedOptionsDefaults: AdvancedOptions;
